1a91bc306cc910ba1d65906d23d9dc237292ebec
jenzie68 <jacecabalo@gmail.com>
2024-03-04 19:10:06 +0400

    graph

--- src/index.js
+++ src/index.js
@@ -1,49 +1,40 @@
 import { LinkedList } from "./linked-list.js";
 
-class Node {
-  constructor(vertex) {
-    this.vertex = vertex;
-    this.vertices = new LinkedList();
+("use strict");
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex() {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2,
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1,
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+  }
+
+  print() {
+    for (const [vertex, edges] of Object.entries(this.adjacencyList)) {
+      console.log(`${vertex} -> ${edges.join(", ")}`);
+    }
   }
 }
-
-let knightMoves = () => {
-  let moves = [
-    [2, -1],
-    [2, 1],
-    [1, -2],
-    [1, 2],
-    [-1, -2],
-    [-1, 2],
-    [-2, -1],
-    [-2, 1],
-  ];
-
-  function graph(vertex) {
-    let n = new Node(vertex);
-    let nodeVerti = addVertices(n);
-    return nodeVerti;
-  }
-
-  function addVertices(node, allMoves = []) {
-    for (let i = 0; i < moves.length; i++) {
-      let vertices = [
-        node.vertex[0] + moves[i][0],
-        node.vertex[1] + moves[i][1],
-      ];
-      if (vertices[0] > 0 && vertices[1] > 0) {
-        allMoves.push(vertices);
-        node.vertices.prepend(new Node(vertices));
-      }
-    }
-    return node;
-  }
-
-  return { graph };
-};
-
-let moves = knightMoves();
-let node = moves.graph([0, 0]);
-console.log(node);
-
-export { knightMoves };
